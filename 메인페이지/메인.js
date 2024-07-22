df56c7e851aaa5c07f2ca248010aa863b7b9544c
internya <wn1234wn@naver.com>
2024-07-22 17:08:21 +0900

    메인페이지
    배너 및 로그인시 글씨만 수정하면 됨

--- 메인페이지/메인.js
+++ 메인페이지/메인.js
@@ -4,4 +4,37 @@ document.addEventListener("DOMContentLoaded", function () {
     e.preventDefault();
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
+
+  let slider = document.querySelector(".slider");
+  const sliders = document.querySelectorAll(".selider li");
+
+  let currentSlide = 0;
+  let slideWidth = sliders[0].clientWidth;
+
+  function foToSlide(index) {
+    if (index < 0) {
+      index = sliders.length - 1;
+    } else if (index >= sliders.length) {
+      index = 0;
+    }
+    slider.computedStyleMap.transform = `translateX(${-index * slideWidth}px)`;
+
+    currentSlide = index;
+  }
+
+  const interval = setInterval(function () {
+    foToSlide(currentSlide + 1);
+  }, 4000);
+
+  slider.addEventListener("mouseenter", function () {
+    clearInterval(interval);
+  });
+
+  slider.addEventListener(
+    "mouseleave",
+    function () {
+      goToSlide(currentSlide + 1);
+    },
+    4000
+  );
 });
