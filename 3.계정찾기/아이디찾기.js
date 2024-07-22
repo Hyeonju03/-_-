91386729ddd5b6d103bcd8c4a50746112ac5f8ae
psy0525 <shineeluv525@gmail.com>
2024-07-22 18:24:11 +0900

    아이디찾기
    css 마무리 js 진행 예정

--- 3.계정찾기/아이디찾기.js
+++ 3.계정찾기/아이디찾기.js
@@ -10,6 +10,26 @@ function handleClick(event) {
   // 클릭한 div만 "click"클래스 추가
   event.target.classList.add("click");
 }
-nonClick.forEach((e) => {
-  e.addEventListener("click", handleClick);
-});
+// nonClick.forEach((e) => {
+//   e.addEventListener("click", handleClick);
+// });
+const modalBtn = document.getElementById("modalBtn");
+modalBtn.addEventListener("click", handleClick);
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const modalBtn = document.getElementById("modalBtn");
+//   const modal = document.getElementById("q_Modal");
+//   const closeBtn = document.getElementById("closeBtn");
+
+//   function toggleModal() {
+//     modal.classList.toggle("show");
+//   }
+//   modalBtn, this.addEventListener("click", toggleModal);
+//   closeBtn, this.addEventListener("click", toggleModal);
+
+//   window.addEventListener("click", function (evernt) {
+//     if (evernt.target == modal) {
+//       toggleModal();
+//     }
+//   });
+// });
