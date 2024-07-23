fbaddafc174d59b4030bf042c73374c016acb136
internya <wn1234wn@naver.com>
2024-07-24 08:53:46 +0900

    main
    유의사항 추가

---
+++ 유의사항/유의사항.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const clickTop = document.querySelector(".gotoTop");
+  clickTop.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
