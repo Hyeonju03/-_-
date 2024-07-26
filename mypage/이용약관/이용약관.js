8935858b3d844ae625bb862731f498b51f05d060
psy0525 <shineeluv525@gmail.com>
2024-07-26 11:14:13 +0900

    취득자격증
    아직 html 하는중

--- mypage/이용약관/이용약관.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const clickTop = document.querySelector(".gotoTop");
-  clickTop.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
-});
