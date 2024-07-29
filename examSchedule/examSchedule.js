c46548f37293b30f55a562753d27c9127db65f9f
internya <wn1234wn@naver.com>
2024-07-29 13:58:21 +0900

    examSchedule
    .

--- examSchedule/examSchedule.js
+++ examSchedule/examSchedule.js
@@ -5,56 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
   });
 
-  // ///////////////////////////////////////////////////
-
-  // localStorage에서 시험 데이터 가져오기
-  const examData = JSON.parse(localStorage.getItem("examData"));
-
-  // 테이블의 tbody 요소 참조
-  const tableBody = document.querySelector("#examTable tbody");
-
-  // 검색 버튼 클릭 시 호출되는 함수
-  function searchExams() {
-    const selectedType = document.getElementById("typeCertificate").value;
-    const startDate = new Date(document.getElementById("startDate").value);
-    const endDate = new Date(document.getElementById("endDate").value);
-
-    // 테이블 초기화
-    tableBody.innerHTML = "";
-
-    // examData를 순회하며 조건에 맞는 시험 일정을 테이블에 추가
-    for (let key in examData) {
-      if (examData.hasOwnProperty(key)) {
-        const exam = examData[key];
-        const examDate = new Date(exam.시험일);
-
-        // 선택한 종목이나 시작일/종료일 범위에 맞는 데이터만 테이블에 추가
-        if (
-          (selectedType === "-" || exam.과목 === selectedType) &&
-          examDate >= startDate &&
-          examDate <= endDate
-        ) {
-          const row = document.createElement("tr");
-          row.innerHTML = `
-        <td>${exam.시험일}</td>
-        <td><a href="${exam.링크}" target="_blank">${exam.과목}</a></td>
-        <td>${exam.접수}</td>
-        <td>${exam.수험표}</td>
-        <td>${exam.결과}</td>
-      `;
-          tableBody.appendChild(row);
-        }
-      }
-    }
-
-    // 결과가 없는 경우 메시지 표시
-    if (tableBody.innerHTML === "") {
-      const row = document.createElement("tr");
-      row.innerHTML = `<td colspan="5">해당 기간과 종목에 맞는 시험 일정이 없습니다.</td>`;
-      tableBody.appendChild(row);
-    }
-  }
-
   ////////////////////////////////// 로그인 관련 ////////////////////////////////////
 
   //로그인 상태 여부
